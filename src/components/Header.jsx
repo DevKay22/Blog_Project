64f4e52e945fb3ca3a3ed300216f3f8f923de3a1
Kayode <drkay4sure@gmail.com>
2024-01-15 19:50:14 +0100

    router set

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FaBars} from 'react-icons/fa';
+import {AiOutlineClose} from 'react-icons/ai';
+
+import logo from '../images/logo.png'
+
+const Header = () => {
+  return (
+   <nav>
+    <div className='container nav__container'>
+      <Link to="/" className='nav_logo'>
+        <img src={logo} alt="" />
+
+      </Link>
+
+      <ul>
+        <li><Link to='/profile'>Kayode</Link></li>
+        <li><Link to='/profile'>Create</Link></li>
+        <li><Link to='/profile'>Authors</Link></li>
+        <li><Link to='/profile'>Logout</Link></li>
+
+      </ul>
+
+      <button className='toggleBtn nav_toggle'><AiOutlineClose/></button>
+
+
+    </div>
+   </nav>
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Layout from './components/Layout';
+import ErrorPage from './pages/Errorpage';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import UserProfile from './pages/UserProfile';
+import CategoryPost from './pages/CategoryPost';
+import EditPost from './pages/EditPost';
+import AuthorPost from './pages/AuthorPost';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Authors from './pages/Authors';
+import CreatePost from './pages/CreatePost';
+import Logout from './pages/Logout';
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home />}, 
+      {path: "post/:id", element: <PostDetail/>},
+      {path: "profile/:id", element: <UserProfile/>},
+      {path: "post/categories/:category", element: <CategoryPost/>},
+      {path: "post/:id/edit", element: <EditPost/>},
+      {path: "profile/:id", element: <AuthorPost/>},
+      {path: "myposts/:id", element: <Dashboard/>},
+      {path: "register", element: <Register/>},
+      {path: "login", element: <Login/>},
+      {path: "authors", element: <Authors/>},
+      {path: "create", element: <CreatePost/>},
+      {path: "logout", element: <Logout/>}
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
